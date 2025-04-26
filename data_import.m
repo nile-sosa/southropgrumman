15d3a365f3adc2b5f9bd4dc293f8dfa58722e2cd
Niles Alexis <nalexis@usc.edu>
2025-04-26 13:53:33 -0700

    ams

--- data_import.m
+++ data_import.m
@@ -1,2 +1,11 @@
-imported_strain = load('Data\Layup1\L1_S11_F\StrainData\L1_S11_F00_DAT.mat');
-plot(imported_strain.strain1);
+clc; clear;
+
+import = load('Data\Layup1\L1_S11_F\PZT-data\L1S11_0_0.mat');
+
+n
+
+figure;
+hold on;
+plot(import.coupon.path_data(1).signal_actuator);
+plot(import.coupon.path_data(1).signal_sensor);
+hold off;
