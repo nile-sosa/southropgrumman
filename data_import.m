2987898fdc748ccd3dd1315b63605234d90ad3c6
wanlitusc <wanlit@usc.edu>
2025-04-26 13:49:04 -0700

    updated data_import for strain
    Example import for one set of actuator, A, M, and S

--- data_import.m
+++ data_import.m
@@ -1,2 +1,24 @@
-imported_strain = load('Data\Layup1\L1_S11_F\StrainData\L1_S11_F00_DAT.mat');
-plot(imported_strain.strain1);
+%% Test import
+
+a1 = load('Data\Layup1\L1_S11_F\StrainData\L1_S11_F00_DAT.mat');
+a2 = load('Data\Layup1\L1_S11_F\StrainData\L1_S11_F01_STRAIN_A_DAT.mat');
+a3 = load('Data\Layup1\L1_S11_F\StrainData\L1_S11_F01_STRAIN_M_DAT.mat');
+a4 = load('Data\Layup1\L1_S11_F\StrainData\L1_S11_F01_STRAIN_S_DAT.mat');
+
+% Plot all 4 datasets
+figure;
+subplot(2,2,1);
+plot(a1.strain1);
+title('L1_S11_F00');
+
+subplot(2,2,2);
+plot(a2.strain1);
+title('L1_S11_F01_A');
+
+subplot(2,2,3);
+plot(a3.strain1);
+title('L1_S11_F01_M');
+
+subplot(2,2,4);
+plot(a4.strain1);
+title('L1_S11_F01_S');
